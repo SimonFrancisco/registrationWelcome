a0a14d200d7bf60c405c071a8a305f18786e2d34
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2023-09-24 05:25:24 +0700

    init commit

--- app/src/main/java/com/example/welcomehome/MainActivity.kt
+++ app/src/main/java/com/example/welcomehome/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.welcomehome
 
-import android.app.ActionBar.LayoutParams
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     //creating a variable for button "Register"
@@ -16,9 +20,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var inputName : EditText
     private lateinit var inputSurname : EditText
     private lateinit var inputBirthday : EditText
-    private lateinit var inputPassword : EditText
-    private lateinit var inputConfPassword : EditText
+    private lateinit var inputPasswordText : TextInputEditText
+    private lateinit var inputConfPasswordText : TextInputEditText
+    private lateinit var inputPasswordLayout : TextInputLayout
+    private lateinit var inputConfPasswordLayout : TextInputLayout
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,8 +41,29 @@ class MainActivity : AppCompatActivity() {
         inputName = findViewById(R.id.inputName)
         inputSurname = findViewById(R.id.inputSurname)
         inputBirthday = findViewById(R.id.inputBirthday)
-        inputPassword = findViewById(R.id.inputPassword)
-        inputConfPassword = findViewById(R.id.inputConPassw)
+        inputPasswordText = findViewById(R.id.inputPasswordText)
+        inputConfPasswordText = findViewById(R.id.inputConPasswordText)
+        inputPasswordLayout = findViewById(R.id.inputPasswordLayout)
+        inputConfPasswordLayout = findViewById(R.id.inputConPasswordLayout)
+
+    // Date Picker
+        inputBirthday.setOnClickListener{
+            val instanceCalendar = Calendar.getInstance()
+            val year = instanceCalendar.get(Calendar.YEAR)
+            val month = instanceCalendar.get(Calendar.MONTH)
+            val day = instanceCalendar.get(Calendar.DAY_OF_MONTH)
+            val dialogDatePicker = DatePickerDialog(this,
+                {view, yearChoose, monthofYear, dayOfMonth ->
+                    val date = (dayOfMonth.toString() + "." + (monthofYear + 1) + "." + year)
+                    inputBirthday.setText(date)
+
+                },
+                year,
+                month,
+                day
+            )
+            dialogDatePicker.show()
+        }
     // Calling second screen
 
         register = findViewById(R.id.buttonRegister)
@@ -52,18 +80,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
     private fun validate(): Boolean {
         val name: String = inputName.text.toString()
         val surname: String = inputSurname.text.toString()
         val birthday: String = inputBirthday.text.toString()
-        val password: String = inputPassword.text.toString()
-        val confpassword: String = inputConfPassword.text.toString()
+        val password: String = inputPasswordText.text.toString()
+        val confpassword: String = inputConfPasswordText.text.toString()
 
     //validation
         if(name.isEmpty() || name.length<2){
             showMessage(inputName, "More than 2 characters needed")
-
             return false
         }
         else if (surname.isEmpty() || surname.length<2){
@@ -71,24 +97,38 @@ class MainActivity : AppCompatActivity() {
             return false
 
         }
-        else if(password.isEmpty() || !password.contains("@")){
-            showMessage(inputPassword, "Weak Password")
+        else if (birthday.isEmpty()){
+            showMessage(inputBirthday, "This can't be empty")
+            return false
+
+        }
+        else if(password.isEmpty() || !password.contains(".*[@#$%^&].*".toRegex())){
+            inputBirthday.error = null // // Necessary to make the error in InputBirthday drop
+            showMessageTextInput(inputPasswordLayout, "Use at least " +
+                    "one special character(@#$%^&)")
             return false
 
         }
         else if(confpassword.isEmpty() || confpassword!=password){
-            showMessage(inputConfPassword,"Password doesn't match")
+            inputPasswordLayout.error = null // Necessary to make the error in InputPassowordL drop
+            showMessageTextInput(inputConfPasswordLayout,"Password doesn't match")
             return false
 
         }
         else{
+            inputConfPasswordLayout.error = null // Error will drop here
             return true
         }
 
     }
-    private fun showMessage(input:EditText,message:String){
-        input.error = message
+    private fun showMessage(input:EditText,messageEditText:String){
+        input.error = messageEditText
         input.requestFocus()
+    }
+    private fun showMessageTextInput(input: TextInputLayout, messageLayout:String){
+        input.error = messageLayout
+        input.requestFocus()
+
     }
 
 
