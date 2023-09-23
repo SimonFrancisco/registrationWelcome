0c8a2bd4b7df96662d53944eda9d053d492140bc
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2023-09-23 21:25:09 +0700

    init commit

--- app/src/main/java/com/example/welcomehome/MainActivity.kt
+++ app/src/main/java/com/example/welcomehome/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     //creating a variable for button "Register"
-    private lateinit var register: View
-
+    private lateinit var register: Button
     //creating variables for validation
     private lateinit var inputName : EditText
     private lateinit var inputSurname : EditText
@@ -22,50 +22,67 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     // Removes warnings on deprecated methods
         @Suppress("DEPRECATION")
 
     //Hide StatusBar
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-    // Calling second screen
-        register = findViewById(R.id.buttonRegister)
-        register.setOnClickListener{
-            validate()
-            val intent = Intent(this,GreetingsActivity::class.java)
-            startActivity(intent)
-            
 
-        }
     //finding Validation
         inputName = findViewById(R.id.inputName)
         inputSurname = findViewById(R.id.inputSurname)
         inputBirthday = findViewById(R.id.inputBirthday)
         inputPassword = findViewById(R.id.inputPassword)
         inputConfPassword = findViewById(R.id.inputConPassw)
+    // Calling second screen
+
+        register = findViewById(R.id.buttonRegister)
+        register.setOnClickListener{
+            val check: Boolean = validate()
+            if(check){
+                val intent = Intent(this,GreetingsActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                return@setOnClickListener
+            }
+        }
 
     }
 
-    private fun validate(){
-        var name: String = inputName.text.toString()
-        var surname: String = inputSurname.text.toString()
-        var birthday: String = inputBirthday.text.toString()
-        var password: String = inputPassword.text.toString()
-        var confpassword: String = inputConfPassword.text.toString()
+
+    private fun validate(): Boolean {
+        val name: String = inputName.text.toString()
+        val surname: String = inputSurname.text.toString()
+        val birthday: String = inputBirthday.text.toString()
+        val password: String = inputPassword.text.toString()
+        val confpassword: String = inputConfPassword.text.toString()
 
     //validation
-
         if(name.isEmpty() || name.length<2){
             showMessage(inputName, "More than 2 characters needed")
+
+            return false
         }
         else if (surname.isEmpty() || surname.length<2){
-            showMessage(inputName,"More than 2 characters needed")
+            showMessage(inputSurname,"More than 2 characters needed")
+            return false
+
         }
         else if(password.isEmpty() || !password.contains("@")){
             showMessage(inputPassword, "Weak Password")
+            return false
+
         }
         else if(confpassword.isEmpty() || confpassword!=password){
             showMessage(inputConfPassword,"Password doesn't match")
+            return false
+
+        }
+        else{
+            return true
         }
 
     }
